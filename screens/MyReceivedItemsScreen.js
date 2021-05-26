ec55d3648288821b2ef7159f9e8c81dc61a908f3
KingAgastya <63898735+KingAgastya@users.noreply.github.com>
2021-05-26 12:58:20 +0530

    Update MyReceivedItemsScreen.js

--- screens/MyReceivedItemsScreen.js
+++ screens/MyReceivedItemsScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {View, StyleSheet, Text, FlatList} from 'react-native'
 import {ListItem} from 'react-native-elements'
 import db from '../config'
+import MyHeader from '../components/MyHeader'
+import firebase from 'firebase'
 
 export default class MyReceivedItemsScreen extends React.Component{
     constructor(){
